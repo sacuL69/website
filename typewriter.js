cb1c9cb81cb19e283cd8cf5d847cd9f43a807382
sacuL69 <154366244+sacuL69@users.noreply.github.com>
2023-12-19 19:26:12 +0100

    Add files via upload

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+var app = document.getElementById('content');
+var typewriter = new Typewriter(app, {
+    loop: false,
+    delay: 50
+});
+
+var text = `
+<font color="19ff00">root@localhost</font><font color="#ffffff">:</font><font color="#00ffff">~</font><font color="#ffffff">$ cd website/about_me</font><br/>
+<font color="19ff00">root@localhost</font><font color="#ffffff">:</font><font color="#00ffff">~</font><font color="#ffffff">$ cat introduction.txt</font><br/><br/>
+>name: lucas<br/> >age: 20<br/> >gender: male<br/> >hobbys: gym<br/> >occupation: sysadmin<br/><br/>
+>find me on my socials: <a href="https://sacul.xyz" class="blink-animation">github</a>`;
