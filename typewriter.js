bd7c3304fbeb3a892f19fa086896f3bbb3323264
sacuL <154366244+sacuL69@users.noreply.github.com>
2023-12-19 20:47:40 +0100

    Update typewriter.js
    updated github link

--- typewriter.js
+++ typewriter.js
@@ -8,4 +8,4 @@ var text = `
 <font color="19ff00">root@localhost</font><font color="#ffffff">:</font><font color="#00ffff">~</font><font color="#ffffff">$ cd website/about_me</font><br/>
 <font color="19ff00">root@localhost</font><font color="#ffffff">:</font><font color="#00ffff">~</font><font color="#ffffff">$ cat introduction.txt</font><br/><br/>
 >name: lucas<br/> >age: 20<br/> >gender: male<br/> >hobbys: gym<br/> >occupation: sysadmin<br/><br/>
->find me on my socials: <a href="https://sacul.xyz" class="blink-animation">github</a>`;
+>find me on my socials: <a href="https://github.com/sacuL69" class="blink-animation">github</a>`;
