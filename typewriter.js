f14115d01936d0faa813f6bfe6313d424fc49852
sacuL <154366244+sacul-xyz@users.noreply.github.com>
2024-04-13 01:08:32 +0200

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -7,5 +7,5 @@ var typewriter = new Typewriter(app, {
 var text = `
 <font color="19ff00">root@localhost</font><font color="#ffffff">:</font><font color="#00ffff">~</font><font color="#ffffff">$ cd website/about_me</font><br/>
 <font color="19ff00">root@localhost</font><font color="#ffffff">:</font><font color="#00ffff">~</font><font color="#ffffff">$ cat introduction.txt</font><br/><br/>
->name: lucas<br/> >age: 20<br/> >gender: male<br/> >hobbys: gym<br/> >occupation: sysadmin<br/><br/>
+>name: lucas<br/> >age: 21<br/> >gender: male<br/> >hobbys: gym<br/> >occupation: sysadmin<br/><br/>
 >see also: <a href="https://github.com/sacul-xyz" class="blink-animation">github</a> <a href="mailto:contact@sacul.xyz" class="blink-animation">e-mail</a>`;
